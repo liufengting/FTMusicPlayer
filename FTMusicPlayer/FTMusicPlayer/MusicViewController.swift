a221a85b97560a0e7da934947d71a241c7568364
liufengting <wo157121900@me.com>
2016-11-09 23:25:27 +0800

    q

--- FTMusicPlayer/FTMusicPlayer/MusicViewController.swift
+++ FTMusicPlayer/FTMusicPlayer/MusicViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 class MusicViewController: UIViewController {
 
@@ -34,6 +36,85 @@ class MusicViewController: UIViewController {
     @IBAction func rightButtonTapped(_ sender: UIBarButtonItem) {
         
     }
+    
+    
+    
+    lazy var musicPlayer : AVPlayer = {
+       let player = AVPlayer()
+        
+        
+        return player
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    
+//    -(void)playWithUrl:(MusicModel*)model
+//    {
+//    AVPlayerItem *item = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:model.url]];
+//    
+//    //替换当前音乐资源
+//    [self.player replaceCurrentItemWithPlayerItem:item];
+//    
+//    //刷新界面UI
+//    [self reloadUI:model];
+//    
+//    //监听音乐播放完成通知
+//    [self addNSNotificationForPlayMusicFinish];
+//    
+//    //开始播放
+//    [self.player play];
+//    
+//    //监听播放器状态
+//    [self addPlayStatus];
+//    
+//    //监听音乐缓冲进度
+//    [self addPlayLoadTime];
+//    
+//    //监听音乐播放的进度
+//    [self addMusicProgressWithItem:item];
+//    
+//    //记录当前播放音乐的索引
+//    self.currentIndex = [model.Id integerValue];
+//    self.currentModel = model;
+//    
+//    //音乐锁屏信息展示
+//    [self setupLockScreenInfo];
+//    
+//    }
+
+    func playMusic(urlString: String) {
+        
+        let item = AVPlayerItem(url: URL(string: urlString)!)
+        
+        self.musicPlayer.replaceCurrentItem(with: item)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
